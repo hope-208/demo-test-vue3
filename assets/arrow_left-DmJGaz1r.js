e9cddbe705b96d72ea6028618b5485b1b8108d8c
Nadezhda Telepenina <Telepenina.N.S@yandex.ru>
2024-06-13 09:19:44 +0400

    Updates

--- assets/arrow_left-DmJGaz1r.js
+++
@@ -1,1 +0,0 @@
-import{o as e,m as t,e as o}from"./index-B1SV3XBQ.js";const n={xmlns:"http://www.w3.org/2000/svg",width:"24",height:"24",fill:"none"},r=o("path",{stroke:"#000","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"2",d:"M19 12H5l7 7m0-14L8 9"},null,-1),s=[r];function c(l,a){return e(),t("svg",n,[...s])}const i={render:c};export{i as default,c as render};
--- assets/chevron_left-CMxYmzJd.js
+++
@@ -1,1 +0,0 @@
-import{o as e,m as t,e as o}from"./index-B1SV3XBQ.js";const n={xmlns:"http://www.w3.org/2000/svg",width:"24",height:"24",fill:"none"},r=o("path",{stroke:"#000","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"2",d:"m14 16-4-4 4-4"},null,-1),s=[r];function c(d,i){return e(),t("svg",n,[...s])}const a={render:c};export{a as default,c as render};
